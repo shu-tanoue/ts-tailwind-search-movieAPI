c68c7b0e9e04d3bb04d4eeee4777c7dc9345403a
Shu <stbook.syu@icloud.com>
2023-05-15 17:41:26 +0900

    first commit

---
+++ components/MovieSearch.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { useState } from "react";
+
+export const MovieSearch = () => {
+  const [movie, setMovies] = useState([]);
+  return <div>MovieSearch</div>;
+};
+
+export default MovieSearch;
